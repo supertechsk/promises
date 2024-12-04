e435ccc43dc07e2a9fdb98bf95253ad19b0d98ff
supertechsk <sachinkumar1852sk@gmail.com>
2024-12-04 21:36:13 +0530

    Add files via upload

---
+++ promises.js
@@ -0,0 +1,21 @@
+//*Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
+
+ function resolvePromise(n) {
+    const promise = new Promise((resolve , reject) =>{
+        setTimeout(() => {
+
+            resolve(`Resolve after ${n} second`);
+
+        }, n * 1000);
+    })
+    return promise;
+        
+}
+
+async function result() {
+   let data =  await resolvePromise(2);
+    console.log(data)
+  
+}
+result();
+
